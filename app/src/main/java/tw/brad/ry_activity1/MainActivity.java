bb487c47249a6cedf95cb796c3c6a72bdfcef08e
Brad Chao <brad@brad.tw>
2018-07-23 10:42:27 +0800

    v1

--- app/src/main/java/tw/brad/ry_activity1/MainActivity.java
+++ app/src/main/java/tw/brad/ry_activity1/MainActivity.java
@@ -1,7 +1,11 @@
 package tw.brad.ry_activity1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("brad", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("brad", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("brad", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("brad", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("brad", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("brad", "onRestart");
+    }
+
+    public void test1(View view) {
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        //Toast.makeText(this, "One more", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void test2(View view) {
+        Intent intent = new Intent(this, Page2Activity.class);
+        startActivity(intent);
     }
 }
